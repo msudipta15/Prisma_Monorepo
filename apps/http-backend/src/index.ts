dc6796adcfd2be02d781bfdc6c503d0477fd49cf
Sudipta Mondal <techtv70@gmail.com>
2025-05-03 20:32:18 +0530

    success , with node 23 monorepo working fine

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,23 @@
 import express from "express";
-
+import { prismaClient } from "@repo/db/client";
 const app = express();
+
+app.use(express.json());
+app.post("/signup", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    await prismaClient.user.create({
+      data: {
+        username: username,
+        password: password,
+      },
+    });
+
+    res.json({ msg: "done" });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(3001);
